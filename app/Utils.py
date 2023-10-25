af60bbd6bc98c45f43254ddddd83bd5323e1a99d
SoulaimenHammami <soulaimen@iovision.io>
2023-10-25 17:57:48 +0100

    update utils

--- app/Utils.py
+++ app/Utils.py
@@ -3,12 +3,12 @@ COGNITO_region =os.environ['COGNITO_region']
 COGNITO_userPoolId =os.environ['COGNITO_userPoolId']
 COGNITO_client_id =  os.environ['COGNITO_client_id']
 
-PSQL_DATABASE = os.environ.get("PSQL_DATABASE")
-PSQL_HOST = os.environ.get("PSQL_HOST")
-PSQL_PASSWORD = os.environ.get("PSQL_PASSWORD")
-PSQL_PORT = os.environ.get("PSQL_PORT")
-PSQL_USER = os.environ.get("PSQL_USER")
-CONNECTION_MAX_OVERFLOW = os.environ.get("CONNECTION_MAX_OVERFLOW")
-CONNECTION_POOL_SIZE = os.environ.get("CONNECTION_POOL_SIZE")
+PSQL_DATABASE = os.environ["PSQL_DATABASE"]
+PSQL_HOST = os.environ["PSQL_HOST"]
+PSQL_PASSWORD = os.environ["PSQL_PASSWORD"]
+PSQL_PORT = os.environ["PSQL_PORT"]
+PSQL_USER = os.environ["PSQL_USER"]
+CONNECTION_MAX_OVERFLOW = os.environ["CONNECTION_MAX_OVERFLOW"]
+CONNECTION_POOL_SIZE = os.environ["CONNECTION_POOL_SIZE"]
 
 database_url = f'postgresql://{PSQL_USER}:{PSQL_PASSWORD}@{PSQL_HOST}:{PSQL_PORT}/{PSQL_DATABASE}'
